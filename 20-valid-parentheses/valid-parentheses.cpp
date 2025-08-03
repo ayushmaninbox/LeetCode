b0a5b52012896c26b4f1b82754eceeb29c416370
Ayushman Mohapatra <146472718+ayushmaninbox@users.noreply.github.com>
2025-08-03 23:54:07 +0530

    Time: 0 ms (100.00%) | Memory: 8.9 MB (33.01%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.cpp
+++ 20-valid-parentheses/valid-parentheses.cpp
@@ -1,21 +1,54 @@
 class Solution {
 public:
     bool isValid(string s) {
-        while (s.find("()") != string::npos || 
-               s.find("[]") != string::npos || 
-               s.find("{}") != string::npos) 
+        stack<char> st;
+        for (int i = 0; i < s.length(); i++) 
         {
-            for (int i = 0; i < s.length() - 1; i++) 
+            char ch = s[i];
+            if (ch == '(' || ch == '{' || ch == '[') 
             {
-                if ((s[i] == '(' && s[i + 1] == ')' ||
-                     s[i] == '{' && s[i + 1] == '}'||
-                     s[i] == '[' && s[i + 1] == ']')) 
-                     {
-                        s.erase(i, 2);
-                        break;
-                     }
+                st.push(ch);
+            }
+            else if (ch == ')' || ch == '}' || ch == ']') 
+            {
+                if (st.empty()) 
+                {
+                    return false;
+                }
+
+                if ((ch == ')' && st.top() != '(' ||
+                     ch == '}' && st.top() != '{' ||
+                     ch == ']' && st.top() != '['))
+                {
+                    return false;
+                }
+                st.pop();
             }
         }
-        return s.empty();
+        return st.empty();
     }
 };
+
+
+
+// class Solution {
+// public:
+//     bool isValid(string s) {
+//         while (s.find("()") != string::npos || 
+//                s.find("[]") != string::npos || 
+//                s.find("{}") != string::npos) 
+//         {
+//             for (int i = 0; i < s.length() - 1; i++) 
+//             {
+//                 if ((s[i] == '(' && s[i + 1] == ')' ||
+//                      s[i] == '{' && s[i + 1] == '}'||
+//                      s[i] == '[' && s[i + 1] == ']')) 
+//                      {
+//                         s.erase(i, 2);
+//                         break;
+//                      }
+//             }
+//         }
+//         return s.empty();
+//     }
+// };
