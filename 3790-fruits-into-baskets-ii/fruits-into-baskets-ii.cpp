537a0378f9c1298de226e5950360a0b04f38e3f9
Ayushman Mohapatra <146472718+ayushmaninbox@users.noreply.github.com>
2025-08-06 23:58:40 +0530

    Time: 1 ms (61.55%) | Memory: 48.8 MB (91.53%) - LeetSync

--- 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.cpp
+++ 3790-fruits-into-baskets-ii/fruits-into-baskets-ii.cpp
@@ -1,21 +1,40 @@
 class Solution {
 public:
     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
-        vector<bool> used(fruits.size(), false);
-        int not_placed = 0;
-        for (int i = 0; i < fruits.size(); i++) {
-            bool placed = false;
-            for (int j = 0; j < fruits.size(); j++) {
-                if (!used[j] && baskets[j] >= fruits[i]) {
-                    used[j] = true;
-                    placed = true;
+        int n = fruits.size(), alloted = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (fruits[i] <= baskets[j]) {
+                    baskets[j] = -1;
+                    alloted++;
                     break;
                 }
             }
-            if (!placed) {
-                not_placed++;
-            }
         }
-        return not_placed;
+        return n - alloted;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int numOfUnplacedFruits(vector<int>& fruits, vector<int>& baskets) {
+//         vector<bool> used(fruits.size(), false);
+//         int not_placed = 0;
+//         for (int i = 0; i < fruits.size(); i++) {
+//             bool placed = false;
+//             for (int j = 0; j < fruits.size(); j++) {
+//                 if (!used[j] && baskets[j] >= fruits[i]) {
+//                     used[j] = true;
+//                     placed = true;
+//                     break;
+//                 }
+//             }
+//             if (!placed) {
+//                 not_placed++;
+//             }
+//         }
+//         return not_placed;
+//     }
+// };
