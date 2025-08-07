397713d60567a562f841b64e14978ebe862cd60f
Ayushman Mohapatra <146472718+ayushmaninbox@users.noreply.github.com>
2025-08-07 23:43:23 +0530

    Time: 8 ms (11.16%) | Memory: 10.9 MB (8.71%) - LeetSync

--- 9-palindrome-number/palindrome-number.cpp
+++ 9-palindrome-number/palindrome-number.cpp
@@ -1,14 +1,26 @@
 class Solution {
 public:
     bool isPalindrome(int x) {
-        if (x < 0 || (x % 10 == 0 && x != 0)) {
-            return false;
-        }
-        int rev = 0;
-        while (x > rev) {
-            rev = rev * 10 + x % 10;
-            x /= 10;
-        }
-        return x == rev || x == rev / 10; 
+        string s = to_string(x);
+        string rev = s;
+        reverse(rev.begin(), rev.end());
+        return s == rev;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     bool isPalindrome(int x) {
+//         if (x < 0 || (x % 10 == 0 && x != 0)) {
+//             return false;
+//         }
+//         int rev = 0;
+//         while (x > rev) {
+//             rev = rev * 10 + x % 10;
+//             x /= 10;
+//         }
+//         return x == rev || x == rev / 10; 
+//     }
+// };
