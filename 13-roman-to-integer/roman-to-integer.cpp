7636f5af80d28e578ab2c0c63b456f567aa837a5
Ayushman Mohapatra <146472718+ayushmaninbox@users.noreply.github.com>
2025-08-09 10:04:57 +0530

    Time: 16 ms (6.58%) | Memory: 13.5 MB (19.41%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        unordered_map <char, int> roman = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+
+        int num = 0;
+        for (int i = 0; i < s.size(); i++) {
+            if (i + 1 < s.size() && roman[s[i]] < roman[s[i + 1]]) { // smaller before larger case : XL - 50-10=40)
+                num -= roman[s[i]];
+            }
+            else { // larger before smaller case : LX - 50+10=60
+                num += roman[s[i]]; 
+            }
+        }
+        return num;
+    }
+};
