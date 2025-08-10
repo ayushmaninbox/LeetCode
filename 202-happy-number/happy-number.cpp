83f05f00cf57e6e90118e734679c4fb40747319a
Ayushman Mohapatra <146472718+ayushmaninbox@users.noreply.github.com>
2025-08-10 20:11:35 +0530

    Time: 0 ms (100.00%) | Memory: 7.8 MB (86.88%) - LeetSync

---
+++ 202-happy-number/happy-number.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int getSum(int n) {
+        int sum = 0;
+        while (n) {
+            int i = n % 10;
+            sum += i * i;
+            n /= 10; // n = n / 10
+        }
+        return sum;
+    }
+
+    bool isHappy(int n) {
+        for (int i = 0; i < 100; i++) {
+            if (n == 1) {
+                return true;
+            }
+            n = getSum(n);
+        }
+        return false;
+    }
+};
