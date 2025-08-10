2436cf7b025a46b11c7dac3e25eca18d4885d89c
Ayushman Mohapatra <146472718+ayushmaninbox@users.noreply.github.com>
2025-08-11 01:37:05 +0530

    Time: 0 ms (100.00%) | Memory: 19.5 MB (31.50%) - LeetSync

--- 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
+++ 167-two-sum-ii-input-array-is-sorted/two-sum-ii-input-array-is-sorted.cpp
@@ -1,17 +1,39 @@
 class Solution {
 public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-        unordered_map<int, int> twoSums;
-        for (int i = 0; i < nums.size(); i++) {
-            twoSums[nums[i]] = i;
-        }
-
-        for (int i = 0; i < nums.size(); i++) {
-            int diff = target - nums[i]; 
-            if (twoSums[diff] && twoSums[diff] != i ) {
-                return {i + 1, twoSums[diff] + 1};
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        int l = 0, r = numbers.size() - 1;
+        while (l < r) {
+            int sum = numbers[l] + numbers[r];
+            if (sum < target) {
+                l++;
+            }
+            else if (sum > target) {
+                r--;
+            }
+            else {
+                return {l + 1, r + 1};
             }
         }
         return {};
     }
 };
+
+
+
+// class Solution {
+// public:
+//     vector<int> twoSum(vector<int>& nums, int target) {
+//         unordered_map<int, int> twoSums;
+//         for (int i = 0; i < nums.size(); i++) {
+//             twoSums[nums[i]] = i;
+//         }
+
+//         for (int i = 0; i < nums.size(); i++) {
+//             int diff = target - nums[i]; 
+//             if (twoSums[diff] && twoSums[diff] != i ) {
+//                 return {i + 1, twoSums[diff] + 1};
+//             }
+//         }
+//         return {};
+//     }
+// };
