66f29314d18575a4cf389b336233b3e595b7b34f
Ayushman Mohapatra <146472718+ayushmaninbox@users.noreply.github.com>
2025-08-10 20:13:00 +0530

    Time: 0 ms (100.00%) | Memory: 7.7 MB (86.88%) - LeetSync

--- 202-happy-number/happy-number.cpp
+++ 202-happy-number/happy-number.cpp
@@ -11,7 +11,7 @@ public:
     }
 
     bool isHappy(int n) {
-        for (int i = 0; i < 100; i++) {
+        for (int i = 0; i < 99; i++) {
             if (n == 1) {
                 return true;
             }
