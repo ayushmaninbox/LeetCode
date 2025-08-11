572d4204a1920358a2bf23907619e0466f29de74
Ayushman Mohapatra <146472718+ayushmaninbox@users.noreply.github.com>
2025-08-11 16:04:09 +0530

    Time: 0 ms (100.00%) | Memory: 22.8 MB (19.86%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,16 +1,32 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        unordered_map<int, int> hash;
-        for (int i : nums) {
-            hash[i]++;
+        int w = 1;
+        for (int r = 1; r < nums.size(); r++) {
+            if (nums[r] != nums[r - 1]) {
+                nums[w] = nums[r];
+                w++;
+            }
         }
-
-        nums.clear();
-        for (auto &p : hash) {
-            nums.push_back(p.first);
-        }
-        sort(nums.begin(), nums.end());
-        return nums.size();
+        return w;
     }
 };
+
+
+
+// class Solution {
+// public:
+//     int removeDuplicates(vector<int>& nums) {
+//         unordered_map<int, int> hash;
+//         for (int i : nums) {
+//             hash[i]++;
+//         }
+
+//         nums.clear();
+//         for (auto &p : hash) {
+//             nums.push_back(p.first);
+//         }
+//         sort(nums.begin(), nums.end());
+//         return nums.size();
+//     }
+// };
